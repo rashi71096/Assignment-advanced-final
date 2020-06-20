64cb567e89ea0899620537a9ec4f71d1ec45a1b5
unknown <rashi@zaycare.com>
2020-06-20 17:55:48 +0530

    Dummy commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
-import Profile from './Profile';
-import axios from 'axios';
-import Loader from './Loader';
+import React, { Component } from 'react'
+import './App.css'
+import Profile from './Profile'
+import axios from 'axios'
+import Loader from './Loader'
 
 class App extends Component {
-    state={data:[],Loader:true}
-    componentDidMount=async()=>
-   {
-       const res=await axios.get('https://jsonplaceholder.typicode.com/users');
-       //console.log(res.data);
-       
-       this.setState({data:res.data,Loader:false});
-   }
-   removeItem=(id1)=>{
-    
-    console.log(id1);
-    const dataSource = this.state.data.filter(item =>{
+  state = { data: [], Loader: true }
+  componentDidMount = async () => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    //console.log(res.data);
+
+    this.setState({ data: res.data, Loader: false })
+  }
+  removeItem = (id1) => {
+    console.log(id1)
+    const dataSource = this.state.data.filter((item) => {
       return item.id !== id1
     })
     //console.log(dataSource);
-   this.setState({data:[...dataSource]});
-   console.log(dataSource);
-   
-
-  
-   }
-
+    this.setState({ data: [...dataSource] })
+    console.log(dataSource)
+    console.log('DONE')
+  }
 
   render() {
+    if (this.state.Loader) {
+      return <Loader />
+    }
 
-    if(this.state.Loader)
-       {
-           return(
-               <Loader />
-           );
-       }
-       
-    return (
-      
-
-        <Profile data={this.state.data} removeItem={this.removeItem} />
-    
-    );
+    return <Profile data={this.state.data} removeItem={this.removeItem} />
   }
 }
 
-export default App;
+export default App
